e7651c48692b0c7d87b6ea5659d7f881844786b0
Nikhil G <nikhil.gaigawali@gmail.com>
2023-12-30 12:56:32 +0530

    initial commit

---
+++ src/main/java/com/fyproj/featureflagsservice/FeatureFlagsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fyproj.featureflagsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureFlagsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fyproj/featureflagsservice/FeatureFlagsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyproj.featureflagsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureFlagsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
